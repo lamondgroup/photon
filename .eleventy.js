ca5672985767d72d777450e3e6aa721b5a8ce101
lamondgroup <91800970+lamondgroup@users.noreply.github.com>
2021-10-03 16:44:12 +0100

    fixed cms passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/assets/css/main.css');
   eleventyConfig.addPassthroughCopy('./src/assets')
   eleventyConfig.addPassthroughCopy('./src/images')
-  eleventyConfig.addPassthroughCopy('/admin')
+  eleventyConfig.addPassthroughCopy('./src/admin')
 
     return {
       dir: {
